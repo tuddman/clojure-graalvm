9ff4598ee392ab14c14c75560b21239ddb7f4114
tuddman <scott.tudd@gmail.com>
2018-07-20 19:36:10 -0400

    updates to graalvm-1.0.0-rc4

--- get-graalvm.sh
+++ get-graalvm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GRAAL_VERSION=1.0.0-rc3
+GRAAL_VERSION=1.0.0-rc4
 GRAALVM_URL=https://github.com/oracle/graal/releases/download/vm-${GRAAL_VERSION}/graalvm-ce-${GRAAL_VERSION}-linux-amd64.tar.gz \
 GRAALVM_PKG=graalvm-ce-${GRAAL_VERSION}-linux-amd64.tar.gz \
 
